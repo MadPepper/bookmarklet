db03b352392e5e54a2ae35e2810d9435c6ddb09f
madpepper <hyper.pepper+github@gmail.com>
2018-10-30 01:11:24 +0900

    Fix: ShareThisPage.jsで直接クリップボードに取り込むように修正

--- ShareThisPage.js
+++ ShareThisPage.js
@@ -1,6 +1,8 @@
 javascript: (
     function () {
-        var select, title, href;
+        // 選択箇所（なければ何も無し）＋ページタイトル＋リンク
+        // フォーマットはMarkdown形式
+        var sel, title, href;
         if (window.getSelection() == "") {
             sel = "";
         } else {
@@ -8,6 +10,18 @@ javascript: (
         }
         title = "[" + document.title.replace(/\v|\f|\n|\r|\t/g, "") + "]";
         href = "(" + location.href + ")";
-        prompt("LINK", sel + title + href);
+        // prompt("LINK", sel + title + href);
+
+        // クリップボードへコピー
+        // textareaとtextnodeを追加->選択->コピー実行
+        // TODO: 一時的とはいえbodyを書き換えるのをなんとかしたい
+        var textArea, bodyElm;
+        textArea = document.createElement("textarea");
+        textArea.textContent = sel + title + href;
+        bodyElm = document.getElementsByTagName("body")[0];
+        bodyElm.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        bodyElm.removeChild(textArea);
     }
 )();
